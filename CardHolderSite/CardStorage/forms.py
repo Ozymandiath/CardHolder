c89133beebb844227863441916d1c9d0f9711c25
Ozymandiath <103983856+Ozymandiath@users.noreply.github.com>
2022-12-12 18:05:48 +0500

    Добавлен фильтр по атрибутам модели

--- CardHolderSite/CardStorage/forms.py
+++ CardHolderSite/CardStorage/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Card
+
 
 class GeneratorForm(forms.Form):
     END_DATE_CHOICES = [
@@ -10,3 +12,24 @@ class GeneratorForm(forms.Form):
     series = forms.IntegerField(max_value=9999, min_value=1, label="Серия")
     end_date = forms.ChoiceField(choices=END_DATE_CHOICES, label="Дата окончания")
     count_card = forms.IntegerField(min_value=1, label="Кол-во карт", required=True)
+
+
+class SearchForm(forms.ModelForm):
+    class Meta:
+        model = Card
+        fields = [
+            "series",
+            "number",
+            "release_date",
+            "end_date",
+            "status"
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['series'].required = False
+        self.fields['number'].required = False
+        self.fields['release_date'].required = False
+        self.fields['end_date'].required = False
+        self.fields['status'].required = False
+        self.fields['status'].value = True
--- CardHolderSite/CardStorage/views.py
+++ CardHolderSite/CardStorage/views.py
@@ -1,12 +1,14 @@
+import datetime
 import random
 
 from dateutil.relativedelta import relativedelta
+from django.db.models import Q
 from django.shortcuts import render, redirect
-from django.utils import timezone
+from django.utils import timezone, dateformat
 from django.views import View
 from django.views.generic import ListView
 
-from .forms import GeneratorForm
+from .forms import GeneratorForm, SearchForm
 from .models import Card
 
 
@@ -14,6 +16,20 @@ class Storage(ListView):
     template_name = "CardStorage/list_card.html"
     model = Card
     context_object_name = "cards"
+    form = SearchForm
+
+    def get_queryset(self):
+        series = Q(series=self.request.GET.get('series')) if self.request.GET.get('series') else Q()
+        number = Q(number__icontains=self.request.GET.get('number')) if self.request.GET.get('number') else Q()
+        release_date = Q(release_date__gte=self.request.GET.get('release_date')) if self.request.GET.get(
+            'release_date') else Q()
+        end_date = Q(end_date__lte=self.request.GET.get('end_date')) if self.request.GET.get('end_date') else Q()
+        status = Q(status=self.request.GET.get('status')) if self.request.GET.get('status') else Q()
+        if len(self.request.GET):
+            object_list = self.model.objects.filter(series & number & release_date & end_date & status)
+        else:
+            object_list = self.model.objects.all()
+        return object_list
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -22,6 +38,9 @@ class Storage(ListView):
                 i.status = "Активна"
             elif i.status == False:
                 i.status = "Просрочена"
+
+        context["form"] = self.form()
+
         return context
 
 
@@ -36,13 +55,14 @@ class GeneratorCard(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             number_list = list("0123456789")
-            release_date = timezone.now()
+            time_now = dateformat.format(timezone.now(), 'Y-m-d H:i')
+            release_date = datetime.datetime.strptime(time_now, '%Y-%m-%d %H:%M')
             if form.cleaned_data["end_date"] == "Year":
                 end_date = release_date + relativedelta(year=1)
             elif form.cleaned_data["end_date"] == "Six-months":
                 end_date = release_date + relativedelta(months=6)
             elif form.cleaned_data["end_date"] == "Month":
-                end_date = release_date + relativedelta(month=1)
+                end_date = release_date + relativedelta(months=1)
 
             for _ in range(form.cleaned_data["count_card"]):
                 number = ""
